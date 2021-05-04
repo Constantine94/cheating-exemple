8cf8f6ffad73595cca184a663fcaff14735dd636
Constantine94 <60316326+Constantine94@users.noreply.github.com>
2021-05-04 13:47:49 +0000

    Update mesaj-catre-client.lua

--- inregistreaza-o-comanda/mesaj-catre-client.lua
+++ inregistreaza-o-comanda/mesaj-catre-client.lua
@@ -1,5 +1,5 @@
 
-
+-- Moonloader cand va executa lucrurile, va incepe mereu cu functia main()
 
 function main() -- functia principala
     sampRegisterChatCommand("salute", te_salut) -- inregistreaza comenzi in joc
@@ -7,7 +7,7 @@ function main() -- functia principala
 end
 
 function te_salut() -- functia te_salut
-    sampAddChatMessage("Salut, ce mai faci?") -- trimite pe chat-ul tau mesajul("Salut, ce mai faci?")
+    sampAddChatMessage("Salut, ce mai faci?") -- trimite pe chat-ul tau mesajul("Salut, ce mai faci?". Mesaj ce este vizibil doar tie)
 end
 
 
