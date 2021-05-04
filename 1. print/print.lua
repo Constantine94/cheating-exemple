f6520bc74678cb49943b5db998e08456d9e65b97
Constantine94 <60316326+Constantine94@users.noreply.github.com>
2021-05-04 14:21:14 +0000

    Add files via upload

---
+++ 1. print/print.lua
@@ -0,0 +1,10 @@
+
+-- Primul lucru pe care moonloader il face, e sa citeasca ce se afla in functia main() 
+-- Codul trebuie scris in functia main()
+
+function main()
+    
+    -- Printeaza in consola ta din sampfuncs "Salut"
+    print("Salut")
+
+end
