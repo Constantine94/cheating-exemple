fa0ab7af8ccc4061ebfab9078b04f6edfbc1cd0e
Constantine94 <60316326+Constantine94@users.noreply.github.com>
2021-05-05 12:20:51 +0000

    Add files via upload

---
+++ 4. for-loop/1. for.lua
@@ -0,0 +1,35 @@
+
+-- For loop este folosit pentru a repeta diverse lucruri de un numar stiut de ori
+
+function main()
+
+	-- Exemplu 1
+	 
+	-- numara de la 1, la 5
+	for numar = 1,5 do
+		print(numar)
+	end
+
+	-- output:
+	-- 1
+	-- 2
+	-- 3
+	-- 4
+	-- 5
+	
+	-- Exemplu 2
+	
+	for numar = 1,6 do
+		print("Numarul meu este: ", numar)
+	end
+
+	-- output:
+	-- Numarul meu este: 1
+	-- Numarul meu este: 2
+	-- Numarul meu este: 3
+	-- Numarul meu este: 4
+	-- Numarul meu este: 5
+	-- Numarul meu este: 6
+
+end 
+
