ac85ed01e4ee0f717ea3187fd879d768380f0732
Constantine94 <60316326+Constantine94@users.noreply.github.com>
2021-05-05 12:11:05 +0000

    Add files via upload

---
+++ 4.for-loop/2. for.lua
@@ -0,0 +1,46 @@
+
+function main()
+	
+	-- Exemplu 1
+
+	-- aici numara din 2 in 2 
+	for aici_pui_orice_nume_vrei_tu = 1, 6, 2 do 
+		print(aici_pui_orice_nume_vrei_tu)
+	end
+	
+	-- output:
+	-- 1
+	-- 3
+	-- 5
+
+	-- Exemplu 2 
+	
+	-- aici numara din 5 in 5 pana la 20 
+	for orice_nume = 0, 20, 5 do 
+		print(orice_nume)
+	end
+
+	-- Output:
+	-- 0
+	-- 5
+	-- 10
+	-- 15
+	-- 20
+
+	-- Exemplu 3
+
+	-- aici numara descrescator de la 6 spre 1 din 1 in 1
+	for orice = 6,1,-1 do 
+		print(orice)
+	end
+
+	-- output:
+	-- 6
+	-- 5
+	-- 4
+	-- 3
+	-- 2
+	-- 1
+	
+end
+
