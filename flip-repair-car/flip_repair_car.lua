0b539fd202e84543e1f0532500f7d273d85ee4df
Constantine94 <60316326+Constantine94@users.noreply.github.com>
2021-06-07 17:39:35 +0000

    Add files via upload

---
+++ flip-repair-car/flip_repair_car.lua
@@ -0,0 +1,40 @@
+local key = require 'vkeys'
+
+function message(msg)
+    sampAddChatMessage("{ff0000}Me: {ffffff}" .. msg)
+end
+
+function main()
+    while true do 
+        wait(0)
+        if isKeyDown(key.VK_F2) then -- flip car
+            flip_car()
+            wait(300)
+        end
+        if isKeyDown(key.VK_F3) then -- repair hack
+            repair_car()
+            wait(300)
+        end
+    end
+end
+
+function flip_car()
+    if isCharInAnyCar(playerPed) then
+        local carHandler = storeCarCharIsInNoSave(playerPed) 
+        x, y, z, w = getVehicleQuaternion(carHandler)
+        print(x, y)
+        setVehicleQuaternion(carHandler, -0.0010155386989936 , -0.0031407431233674, z, w)
+    else
+        message("Nu e in masina")
+    end
+end
+
+function repair_car()
+    if isCharInAnyCar(playerPed) then
+        local carHandler = storeCarCharIsInNoSave(playerPed)
+        setCarHealth(carHandler, 999) 
+    else
+        message("Nu e in masina")
+    end
+end 
+
